3cbafcfe15b7220d5344e018534e15c9e95253e1
joma <joomartsatybekov9@gmail.com>
2023-09-25 23:43:36 +0600

    new project

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { Main } from "./components/Main/Main";
+import { Tostify } from "./components/Tostify/Tostify";
+
+
+
+function App() {
+  return (
+    <div className="App">
+
+    <Main />
+    <Tostify />
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,51 @@
+import React, { useState } from 'react';
+import { Carousel, Radio } from 'antd';
+import Telik from '../../images/телик.webp'
+import Telik2 from '../../images/тулик2.webp'
+const contentStyle = {
+  height: '160px',
+  color: '#fff',
+  lineHeight: '160px',
+  textAlign: 'center',
+  background: '#364d79',
+};
+export const Main = () => {
+  const [dotPosition, setDotPosition] = useState('top');
+  const handlePositionChange = ({ target: { value } }) => {
+    setDotPosition(value);
+  };
+  return (
+    <>
+      <Radio.Group
+        onChange={handlePositionChange}
+        value={dotPosition}
+        style={{
+          marginBottom: 8,
+        }}
+      >
+        <Radio.Button value="top">Top</Radio.Button>
+        <Radio.Button value="bottom">Bottom</Radio.Button>
+        <Radio.Button value="left">Left</Radio.Button>
+        <Radio.Button value="right">Right</Radio.Button>
+      </Radio.Group>
+      <Carousel dotPosition={dotPosition}>
+        <div>
+          <h3 >
+                <img src={Telik} alt=""style={contentStyle} />
+          </h3>
+        </div>
+        <div>
+          <h3>
+            <img src={Telik2} alt="" style={contentStyle}/>
+          </h3>
+        </div>
+        <div>
+          <h3 style={contentStyle}>3</h3>
+        </div>
+        <div>
+          <h3 style={contentStyle}>4</h3>
+        </div>
+      </Carousel>
+    </>
+  );
+};
